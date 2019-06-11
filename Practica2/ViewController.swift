068693371ad57a915f4f974e9cd059185e3e4eae
Cecy <icecilia.orozco@alumnos.udg.mx>
2019-06-10 19:53:15 -0500

    Initial Commit

---
+++ Practica2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practica2
+//
+//  Created by MacBook on 6/10/19.
+//  Copyright © 2019 DesarrolloCecy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
